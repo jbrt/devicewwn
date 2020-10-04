ffd1d35c6b989ae20b9e428a68942328038915b2
Christoph Hille <38468371+hille721@users.noreply.github.com>
2020-10-04 11:01:44 +0200

    Create hitachi.py
    support for Hitachi SAN Storage (only for naa6 addresses)

---
+++ devicewwn/manufacturers/hitachi/hitachi.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+"""
+Hitachi WWN
+"""
+
+import binascii
+from devicewwn.wwn import WWN, WWNInvalidError
+
+
+class HitachiWWNError(WWNInvalidError):
+    """
+    Generic Hitachi Exception
+    """
+    def __init__(self, value):
+        super().__init__(f"Invalid Hitachi WWN: {value}")
+
+
+class HitachiWWN(WWN):
+    """
+    Decode WWN from Hitachi
+    """
+
+    def __init__(self, address):
+        super().__init__(address)
+        if self.oui != '00:60:e8':
+            raise HitachiWWNError('This not a Hitachi !')
+
+
+    def _decode_naa6(self):
+        serial = str(int(self.wwn_nodots[10:14], 16))
+        model = 'Hitachi'
+        hve = self.wwn[-5:]
+        self._decode = f'{model} S/N:{serial} HVE:{hve}'
